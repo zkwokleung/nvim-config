9b60887079f373e0bf10ff2b2a3fb81b822d3534
Andrew SZETO <zkwokleung@gmail.com>
2025-07-14 16:54:27 +0800

    feat: removed copilot from helper

--- init.lua
+++ init.lua
@@ -1,52 +1,50 @@
 if vim.loader then
-	vim.loader.enable()
+    vim.loader.enable()
 end
 
 local core = {
-	"core/bootstrap",
-	"core/options",
-	"core/lazy",
-	"core/plugins",
-	"core/autocmd",
-	"core/mappings",
+    "core/bootstrap",
+    "core/options",
+    "core/lazy",
+    "core/plugins",
+    "core/autocmd",
+    "core/mappings",
 }
 
 local lang = {
-	"lang/lsp",
-	"lang/lint",
-	"lang/fmt",
-	"lang/snippet",
-	"lang/comments",
+    "lang/lsp",
+    "lang/lint",
+    "lang/fmt",
+    "lang/snippet",
+    "lang/comments",
 }
 
 local ui = {
-	"ui/commandbar",
-	"ui/comments",
-	"ui/dashboard",
-	"ui/indent",
-	"ui/statusline",
-	"ui/theme",
-	"ui/tree",
+    "ui/commandbar",
+    "ui/comments",
+    "ui/dashboard",
+    "ui/indent",
+    "ui/statusline",
+    "ui/theme",
+    "ui/tree",
 }
 
-local helper = {
-	"helper/copilot",
-}
+local helper = {}
 
 -- Load my configs
 
 for _, v in ipairs(core) do
-	require(v)
+    require(v)
 end
 
 for _, v in ipairs(lang) do
-	require(v)
+    require(v)
 end
 
 for _, v in ipairs(ui) do
-	require(v)
+    require(v)
 end
 
 for _, v in ipairs(helper) do
-	require(v)
+    require(v)
 end
