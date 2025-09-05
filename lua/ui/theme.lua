b77315939b175045daa9500a21349aaefac8e0e8
Andrew SZE-TO <zkwokleung@gmail.com>
2025-09-05 17:02:42 +0800

    feat: updated git sign line blame color

--- lua/ui/theme.lua
+++ lua/ui/theme.lua
@@ -1,1 +1,8 @@
 vim.cmd([[colorscheme onedark]])
+
+-- Gitsigns: color for current line blame virtual text
+vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", {
+    fg = "#5c6370",
+    italic = true,
+    nocombine = true,
+})
