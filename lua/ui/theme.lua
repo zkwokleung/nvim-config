07be9d9bb52d620363b5b7fc12ce0e63a1821d4a
Andrew SZE-TO <zkwokleung@gmail.com>
2025-10-16 11:31:56 +0800

    feat: updated barbar style

--- lua/ui/theme.lua
+++ lua/ui/theme.lua
@@ -6,3 +6,40 @@ vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", {
     italic = true,
     nocombine = true,
 })
+
+-- Barbar: brighten non-current buffers for better legibility
+local dim_buffer_fg = "#9da5b4"
+local dim_buffer_groups = {
+    "BufferInactive",
+    "BufferInactiveIndex",
+    "BufferInactiveIcon",
+    "BufferInactiveMod",
+    "BufferInactiveSign",
+    "BufferInactiveTarget",
+    "BufferInactiveButton",
+    "BufferVisible",
+    "BufferVisibleIndex",
+    "BufferVisibleIcon",
+    "BufferVisibleMod",
+    "BufferVisibleSign",
+    "BufferVisibleTarget",
+    "BufferVisibleButton",
+}
+
+for _, group in ipairs(dim_buffer_groups) do
+    vim.api.nvim_set_hl(0, group, { fg = dim_buffer_fg })
+end
+
+local bold_buffer_groups = {
+    "BufferCurrent",
+    "BufferCurrentIndex",
+    "BufferCurrentIcon",
+    "BufferCurrentMod",
+    "BufferCurrentSign",
+    "BufferCurrentTarget",
+    "BufferCurrentButton",
+}
+
+for _, group in ipairs(bold_buffer_groups) do
+    vim.api.nvim_set_hl(0, group, { fg = "#ffffff", bold = true })
+end
