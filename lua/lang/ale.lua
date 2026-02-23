94b6b5eae6c4275f491b7a4c45396ce86deebeea
Andrew SZE-TO <zkwokleung@gmail.com>
2026-02-23 15:47:20 +0800

    feat: ale use nvim diagnostics api

--- lua/lang/ale.lua
+++ lua/lang/ale.lua
@@ -26,6 +26,7 @@ end
 g.ale_disable_lsp = 1
 g.ale_completion_enabled = 0
 g.ale_linters_explicit = 1
+g.ale_use_neovim_diagnostics_api = 1
 g.ale_fix_on_save = 1
 
 g.ale_linters = build_ale_linters(linters_by_ft)
