4e717c05ffef3d1c0da844b5e033ba8190062bd4
Andrew SZE-TO <zkwokleung@gmail.com>
2026-02-25 13:59:35 +0800

    feat: updated structure for config

--- lua/lang/lint.lua
+++ lua/lang/lint.lua
@@ -1,8 +1,17 @@
 local M = {}
 
+local fn = vim.fn
+
+local lint_config_dir = fn.stdpath("config") .. "/config/lang/lint"
+local markdownlint_config = lint_config_dir .. "/.markdownlint.jsonc"
+local yamllint_config = lint_config_dir .. "/yamllint.yml"
+
 M.ale_linter_options_by_ft = {
     markdown = {
-        markdownlint = "--disable MD013",
+        markdownlint = "--config " .. fn.shellescape(markdownlint_config),
+    },
+    yaml = {
+        yamllint = "-c " .. fn.shellescape(yamllint_config),
     },
 }
 
