d507dceac40992e140399765cebfbc9bb23f3607
Andrew SZE-TO <zkwokleung@gmail.com>
2026-02-26 17:49:04 +0800

    feat: disabled snacks explorer

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -225,7 +225,7 @@ require("lazy").setup({
             -- refer to the configuration section below
             bigfile = { enabled = true },
             dashboard = { enabled = true },
-            explorer = { enabled = true },
+            explorer = { enabled = false },
             indent = { enabled = true },
             input = { enabled = true },
             picker = { enabled = true },
